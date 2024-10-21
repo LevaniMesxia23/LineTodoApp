88a2ac36df5df16b73ae285bf4f781a0d34df2e9
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-22 00:26:51 +0400

    config: add react-i18next

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@clerk/clerk-react','i18next']
+      external: ['@clerk/clerk-react', 'i18next', 'react-i18next']
     }
   },
   optimizeDeps: {
-    include: ['i18next']
-}
-
-})
+    include: ['i18next', 'react-i18next']
+  }
+});
