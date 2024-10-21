ff2b5b8e432c3676a3cfd0678c51bdd91796e528
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-22 00:53:16 +0400

    config: delete build in vide.config

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@clerk/clerk-react',
-        'i18next',
-        'react-i18next',
-        'react-router-dom',
-        '@uidotdev/usehooks',
-        '@tanstack/react-query', 
-        'react',                
-        'react-dom',           
-        'aos',
-        'aos/dist/aos.css'           
-      ]
-    }
-  },
   optimizeDeps: {
     include: ['i18next', 'react-i18next']
   }
