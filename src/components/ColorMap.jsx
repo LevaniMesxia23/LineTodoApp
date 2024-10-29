6272babf8db126e89f6d32d3ae7f3a952a3aa926
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-30 00:26:10 +0400

    style: correct complete

--- src/components/ColorMap.jsx
+++ src/components/ColorMap.jsx
@@ -4,15 +4,15 @@ function ColorMap() {
   return (
     <div className="flex flex-col mb-8 gap-6 w-full">
       <div className="flex gap-4 items-center">
-        <p className="text-green-400 text-[1.5rem] font-semibold">{t("Complete")} </p>
+        <p className=" text-[1.5rem] font-semibold">{t("Complete")} </p>
         <div className="w-6 h-6 bg-green-400 rounded-full animated-infinity"></div>
       </div>
       <div className="flex gap-4 items-center">
-        <p className="text-orange-400 text-[1.5rem] font-semibold">{t("Important")} </p>
+        <p className=" text-[1.5rem] font-semibold">{t("Important")} </p>
         <div className="w-6 h-6 bg-orange-400 rounded-full animated-infinity"></div>
       </div>
       <div className="flex gap-4 items-center">
-        <p className="text-[#9966CC] text-[1.5rem] font-semibold">{t("Complate & Important")} </p>
+        <p className=" text-[1.5rem] font-semibold">{t("Complete & Important")} </p>
         <div className="w-6 h-6 bg-[#9966CC] rounded-full animated-infinity"></div>
       </div>
     </div>
